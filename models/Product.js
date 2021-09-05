36a4b8f0fa54123111861e55476a7b31bb766b33
Vish O <v.opatha@gmail.com>
2021-09-05 11:31:13 +0930

    Add columns to Product model

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price:{
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isInt: true,
+      }
+    },
+    category_id :{
+//       category_id
+
+// Integer.
+
+// References the Category model's id.
+
+    }
   },
   {
     sequelize,
