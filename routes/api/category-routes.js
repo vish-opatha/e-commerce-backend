7ca4a089bd2661a96e398df9331bba4b0a5dc1ee
Vish O <v.opatha@gmail.com>
2021-09-06 16:09:54 +0930

    Add update category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,12 +29,31 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// This route is used to create a new category with the given category name
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// This route is used to update the category name of a given category id
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(
+      {category_name: req.body.category_name},
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    res.status(200).json({message: "Category is updated!"});
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.delete('/:id', (req, res) => {
