2c39c9dc581ce6c1621ec182de0589f2df7e5595
Vish O <v.opatha@gmail.com>
2021-09-06 16:49:37 +0930

    Add select all and delete routes for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
+// Endpoints for /api/tags
+// #################################################
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// This is used to retrieve all the tags
+router.get('/', async (req, res) => {
+  try {
+    const tags = await Tag.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(tags);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.get('/:id', (req, res) => {
@@ -21,8 +29,17 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedTag = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json({message: "Tag is Deleted!"});
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
